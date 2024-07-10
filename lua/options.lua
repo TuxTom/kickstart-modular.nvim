c797a74f91d79f915b7e81c1eb1e73e32c19f1d1
Klöber Thomas <Thomas.Kloeber@elektrobit.com>
2024-07-10 10:48:18 +0200

    Switch to newer font version

--- lua/options.lua
+++ lua/options.lua
@@ -72,7 +72,7 @@ if os:match 'Windows' then
 end
 
 -- Options for Neovide
-vim.o.guifont = 'JetBrainsMono NF:h11:#e-subpixelantialias'
+vim.o.guifont = 'JetBrainsMono Nerd Font:h11:#e-subpixelantialias'
 vim.g.neovide_cursor_animation_length = 0.05
 vim.g.neovide_cursor_trail_size = 0.3
 
