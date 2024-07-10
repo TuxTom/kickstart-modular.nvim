45c204cd935e3b9eebf0aa5249c2b327d04e6d36
Klöber Thomas <Thomas.Kloeber@elektrobit.com>
2024-07-10 15:07:22 +0200

    Use cmp for command line completion (although not fully functional)

--- lua/kickstart/plugins/cmp.lua
+++ lua/kickstart/plugins/cmp.lua
@@ -112,6 +112,29 @@ return {
           { name = 'path' },
         },
       }
+
+      -- cmp.setup.cmdline({ '/', '?' }, {
+      --   mapping = cmp.mapping.preset.cmdline(),
+      --   sources = {
+      --     { name = 'buffer' },
+      --   },
+      -- })
+      --
+      cmp.setup.cmdline(':', {
+        mapping = cmp.mapping.preset.cmdline(),
+        sources = {
+          { name = 'path' },
+          { name = 'cmdline' },
+        },
+        matching = {
+          disallow_fuzzy_matching = false,
+          disallow_fullfuzzy_matching = false,
+          disallow_partial_fuzzy_matching = true,
+          disallow_partial_matching = false,
+          disallow_prefix_unmatching = false,
+          disallow_symbol_nonprefix_matching = false,
+        },
+      })
     end,
   },
 }
