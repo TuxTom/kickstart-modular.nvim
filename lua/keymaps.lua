b620a7d6bdaa40cc7a20627c72037209b3061514
Klöber Thomas <Thomas.Kloeber@elektrobit.com>
2024-07-19 10:23:10 +0200

    Add keymap to grep neovim config

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -31,6 +31,10 @@ vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right win
 vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 
+vim.keymap.set('n', '<leader>sN', function()
+  require('telescope.builtin').live_grep { cwd = vim.fn.stdpath 'config' }
+end, { desc = '[S]earch [N]eovim files by grep' })
+
 -- [[ Basic Autocommands ]]
 --  See `:help lua-guide-autocommands`
 
