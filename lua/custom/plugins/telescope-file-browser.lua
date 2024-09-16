7a50798073bf7b569ab0b348d01426a49b43759f
Klöber Thomas <Thomas.Kloeber@elektrobit.com>
2024-09-16 08:04:23 +0200

    Add keybindings to search for files recursively

--- lua/custom/plugins/telescope-file-browser.lua
+++ lua/custom/plugins/telescope-file-browser.lua
@@ -9,6 +9,12 @@ return {
       vim.keymap.set('n', '<leader>sF', function()
         require('telescope').extensions.file_browser.file_browser { path = '%:p:h', cwd_to_path = true }
       end, { desc = '[S]earch [F]iles from %:p:h' })
+      vim.keymap.set('n', '<leader>sd', function()
+        require('telescope').extensions.file_browser.file_browser { depth = false }
+      end, { desc = '[S]earch files in [D]irectory recursively' })
+      vim.keymap.set('n', '<leader>sD', function()
+        require('telescope').extensions.file_browser.file_browser { depth = false, path = '%:p:h', cwd_to_path = true }
+      end, { desc = '[S]earch files in [D]ir from %:p:h recursively' })
     end,
     cond = not vim.g.vscode,
   },
